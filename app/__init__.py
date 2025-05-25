01b0bfb914c36a0ca85d5472fb900b714e687c04
txmlrd <gungadhisanjaya@gmail.com>
2025-05-25 14:25:33 +0700

    feat: setting all the docker and all the stuff to be able migrate and allign on the deploy

--- app/__init__.py
+++ app/__init__.py
@@ -8,15 +8,10 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # jwt(app)
     db.init_app(app)
     migrate.init_app(app, db)
     bcrypt.init_app(app)
     jwt.init_app(app)
-    
-    # Membuat tabel jika belum ada
-    with app.app_context():
-        db.create_all()  # Ini akan membuat tabel berdasarkan model yang ada
     
     app.register_blueprint(recognition_bp)
     app.register_blueprint(auth_bp)
