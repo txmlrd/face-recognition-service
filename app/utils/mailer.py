5ef689d363f7825c9da1683962a4db9712016491
txmlrd <gungadhisanjaya@gmail.com>
2025-06-26 10:59:53 +0700

    fix: fixing routing mailer auth service

--- app/utils/mailer.py
+++ app/utils/mailer.py
@@ -3,9 +3,11 @@ from flask_mail import Message
 from flask import current_app
 
 def send_email(subject, recipient, html=None, body=None):
-    msg = Message(subject, sender=current_app.config['MAIL_USERNAME'], recipients=[recipient])
-    if html:
-        msg.html = html
-    if body:
-        msg.body = body
-    mail.send(msg)
+    with current_app.app_context():
+        msg = Message(subject, sender=current_app.config['MAIL_USERNAME'], recipients=[recipient])
+        if html:
+            msg.html = html
+        if body:
+            msg.body = body
+        mail.send(msg)
+
